106475aa976db4a5d1407f9ef42cdbbee2f72b67
Anas <anasmeezy@gmail.com>
2026-06-24 16:03:12 +0300

    deployment fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://chatty-production-aa56.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
